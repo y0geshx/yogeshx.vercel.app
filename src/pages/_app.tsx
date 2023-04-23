746496a9bce01293e581369a777bc9397f1f3c71
vibrantifix <92950980+vibrantifix@users.noreply.github.com>
2023-04-23 12:33:36 +0530

    Vercel Web Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,9 @@ import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import NextNProgress from "nextjs-progressbar";
 import { ThemeProvider } from "next-themes";
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -25,3 +28,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
 };
 
 export default trpc.withTRPC(MyApp);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+ 
+export default MyApp;
